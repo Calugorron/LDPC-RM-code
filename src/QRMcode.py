59d88f4634eeb6d56185a11e036ddb7d4207aed7
Calugorron <calugorron@gmail.com>
2025-04-29 10:59:23 +0200

    Added QRM class and build_QRM_circuit

--- src/QRMcode.py
+++ src/QRMcode.py
@@ -1,6 +1,8 @@
 import numpy as np
 import scipy as sp
-from .utils import bin_wt
+import math
+from .utils import bin_wt, get_layout, flatten, int2bin, rank
+import itertools as iter
 
 class QuantumReedMuller:
 
@@ -9,7 +11,8 @@ class QuantumReedMuller:
         self._N = 2**n
         self._rx = rx
         self._rz = rz
-        self._k = np.sum(list(sp.special.binom(n, i) for i in range(rx+1, n-rz)))
+        list_for_k = list(math.comb(n, i) for i in range(rx+1, n-rz))
+        self._k = sum(list_for_k)
         self._dx = 2**(rx+1)
         self._dz = 2**(rz+1) 
         if rx==rz:
@@ -20,6 +23,8 @@ class QuantumReedMuller:
         self._E, self._SX, self._SZ, self._LX, self._LZ = self._generate_code_properties()
 
         self._G = None
+        self._GS = None
+        self._aggregate_matrix = None
 
     def _generate_code_properties(self):
         N=2**self.n
@@ -44,10 +49,88 @@ class QuantumReedMuller:
             if bin_wt(row) >= self._n-self._rz: 
                 SZ.append(E[row])
         return E, SX, SZ, LX, LZ
+    
 
     def gauge_operators(self, polynomials):
-        all_checks= list(range(int(self._N**(1/2))))
-        return None
+        if self._G == None:
+            self._G = []
+        if self._GS == None:
+            self._GS = []
+
+
+        #Function to generate operator from polynomial
+        def gen_pattern(poly, check):
+            pattern = list(range(2**self._n))
+            for i, number in enumerate(pattern):
+                    binary = int2bin(number, self._n)
+                    if binary[poly[0]-1]==check[0] and binary[poly[1]-1]==check[1] and binary[poly[2]-1]==check[2]:
+                        pattern[i]=1
+                    else:
+                        pattern[i]=0
+            return pattern
+        
+        #Gauge logical
+        saved_poly = []
+        saved_check = []
+        complementary_polynomials = []
+        all_checks= list(int2bin(i, 3) for i in range(int(self._N**(1/2))))
+        for poly in polynomials:
+            for check in all_checks:
+                possible_gauge=gen_pattern(poly, check)
+                for j, L in enumerate(self._LZ):
+                    if L.tolist()==possible_gauge:
+                        complementary_polynomials.append(list(range(1, self._n+1)))
+                        for x in poly:
+                            complementary_polynomials[-1].remove(x)
+                        self._G.append(L)
+                        self._GS.append(L)
+                        self._LZ.pop(j)
+                        saved_poly.append(poly)
+                        saved_check.append(check)
+            if poly not in saved_poly:
+                print(f"The polynomial {poly} doesn't result in a logical operator to gauge")
+        #remove complementary logicals
+        for poly in complementary_polynomials:
+            for check in all_checks:
+                possible_logical_to_remove=gen_pattern(poly, check)
+                for j, L in enumerate(self._LZ):
+                    if L.tolist()==possible_logical_to_remove:
+                        self._LZ.pop(j)
+
+        #Create group of gauge that spans stabilizer group + gauge group
+        new_GS = []
+        for i, poly in enumerate(polynomials):
+            for j, check in enumerate(all_checks):
+                if not (poly == saved_poly[i] and check == saved_check[i]):
+                    new_gauge = gen_pattern(poly, check)
+                    for gauge in self._G:
+                        new_GS = self._GS[:]
+                        new_GS.append(new_gauge)
+                        if self.is_stabilizer(new_gauge+gauge) and rank(self._GS)+1==rank(new_GS):
+                            self._GS.append(new_gauge)
+                            break
+        if rank(self._GS) == len(self._SZ)+len(self._G):
+            print("All stabilizers have weight reduced from the gauges")
+
+        #Create aggregate matrix to rebuild stabilizers from gauge
+        if self._aggregate_matrix == None:
+            self._aggregate_matrix = []
+        for stab in self._SZ:
+            weight_stab = sum(stab)
+            weight_gauge = self._d
+            for comb in iter.combinations(range(len(self._GS)), int(weight_stab/weight_gauge)):
+                summed_gauge = [0]*len(stab)
+                for element in comb:
+                    summed_gauge = np.bitwise_xor(summed_gauge, self._GS[element])
+                if not np.any(np.bitwise_xor(summed_gauge, stab)):
+                    row = [0]*len(self._GS)
+                    for element in comb:
+                        row[element]=1
+                    self._aggregate_matrix.append(row)
+                    break
+
+
+        
 
     @property
     def N(self):
@@ -93,6 +176,18 @@ class QuantumReedMuller:
     @property
     def SZ(self):
         return self._SZ
+    
+    @property
+    def G(self):
+        return self._G
+    
+    @property
+    def GS(self):
+        return self._GS
+    
+    @property
+    def aggregate_matrix(self):
+        return self._aggregate_matrix
     
     def is_stabilizer(self, stabilizer):
         propagated_stabilizer = list(stabilizer)
--- src/build_circuit.py
+++ src/build_circuit.py
@@ -1,9 +1,230 @@
 import numpy as np
 import scipy as sp
+import stim
+from typing import List, FrozenSet, Dict
+from scipy.sparse import csc_matrix
 
-from src.utils import get_layout, distribute, show_group, bin_wt
+from src.utils import edge_coloring_bipartite
+
+def build_QRM_circuit(QRM, p, num_repeat, z_basis=True, use_both=False, HZH=False):
+    circuit = stim.Circuit()
+
+    edges, num_colors = edge_coloring_bipartite(np.array(QRM.GS))
+    
+    data_size = QRM.N
+    stab_size = len(QRM.GS)
+    aggregate_matrix = QRM.aggregate_matrix
+
+    #Data and ancilla initialization
+    for i in range(QRM.N):
+        circuit.append("R" if z_basis else "RX", i)
+        circuit.append("X_ERROR" if z_basis else "Z_ERROR", i, p)
+    for i in range(stab_size):
+        circuit.append("R", data_size+i)
+        circuit.append("X_ERROR", data_size+i, p)
+        circuit.append("RX", data_size+stab_size+i)
+        circuit.append("Z_ERROR", data_size+stab_size+i, p)
+    circuit.append("TICK")
+
+    #Z stab SE first round
+    Z_stab_SE = ""
+    X_stab_SE = ""
+    for color in range(num_colors):
+        for edge in edges[color]:
+            Z_stab_SE += f"CNOT {edge[1]} {edge[0]+data_size}\n"
+            Z_stab_SE += f"DEPOLARIZE2({p}) {edge[1]} {edge[0]+data_size}\n"
+            X_stab_SE += f"CNOT {edge[0] + data_size+ stab_size} {edge[1]}\n"
+            X_stab_SE += f"DEPOLARIZE2({p}) {edge[0] + data_size+ stab_size} {edge[1]}\n"
+        Z_stab_SE += f"TICK \n"
+        X_stab_SE += f"TICK \n"
+    circuit += stim.Circuit(Z_stab_SE)
+    circuit += stim.Circuit(X_stab_SE)
+
+    
+    ###########ROUNDS
+    round_circuit = stim.Circuit()
+
+    #Data idling
+    for i in range(QRM.N):
+        round_circuit.append("DEPOLARIZE1", i, p)
 
 
+    #Ancilla measurment and initialization
+    X_ancilla_measurment = ""
+    Z_ancilla_measurment = ""
+    for i in range(stab_size):
+        Z_ancilla_measurment += f"X_ERROR({p}) {data_size+i}\n"
+        Z_ancilla_measurment += f"MR {data_size+i}\n"
+        Z_ancilla_measurment += f"X_ERROR({p}) {data_size+i}\n"
+        X_ancilla_measurment += f"Z_ERROR({p}) {data_size+stab_size+i}\n"
+        X_ancilla_measurment += f"MRX {data_size+stab_size+i}\n"
+        X_ancilla_measurment += f"Z_ERROR({p}) {data_size+stab_size+i}\n"
+    round_circuit += stim.Circuit(Z_ancilla_measurment)
+    round_circuit += stim.Circuit(X_ancilla_measurment)
+    round_circuit.append("TICK")
 
+    Z_stab_SE = ""
+    X_stab_SE = ""
+    for color in range(num_colors):
+        for edge in edges[color]:
+            Z_stab_SE += f"CNOT {edge[1]} {edge[0]+data_size}\n"
+            Z_stab_SE += f"DEPOLARIZE2({p}) {edge[1]} {edge[0]+data_size}\n"
+            X_stab_SE += f"CNOT {edge[0] + data_size+ stab_size} {edge[1]}\n"
+            X_stab_SE += f"DEPOLARIZE2({p}) {edge[0] + data_size+ stab_size} {edge[1]}\n"
+        Z_stab_SE += f"TICK \n"
+        X_stab_SE += f"TICK \n"
+    round_circuit += stim.Circuit(Z_stab_SE)
+    round_circuit += stim.Circuit(X_stab_SE)
+    ######
+
+    circuit += (num_repeat-1)*round_circuit
+
+
+    #Final ancilla measurment
+    final_X_ancilla_measurment = ""
+    final_Z_ancilla_measurment = ""
+    for i in range(stab_size):
+        final_Z_ancilla_measurment += f"X_ERROR({p}) {data_size+i}\n"
+        final_Z_ancilla_measurment += f"M {data_size+i}\n"
+        final_X_ancilla_measurment += f"Z_ERROR({p}) {data_size+stab_size+i}\n"
+        final_X_ancilla_measurment += f"MX {data_size+stab_size+i}\n"
+    circuit += stim.Circuit(final_Z_ancilla_measurment)
+    circuit += stim.Circuit(final_X_ancilla_measurment)
+    
+    
+    detector_str = ""
+    for round_num in range(num_repeat):
+        if round_num==0:
+            if z_basis:
+                for row in aggregate_matrix:
+                    detector_str += "DETECTOR "
+                    for i, value in enumerate(row):
+                        if value == 1:
+                            detector_str += f"rec[{-((num_repeat-round_num)*stab_size*2)+i}] "
+                    detector_str += "\n"
+            else:
+                for row in aggregate_matrix:
+                    detector_str += "DETECTOR "
+                    for i, value in enumerate(row):
+                        if value == 1:
+                            detector_str += f"rec[{-((num_repeat-round_num)*stab_size*2)+i+stab_size}] "
+                    detector_str += "\n"
+        else:
+            if z_basis:
+                for row in aggregate_matrix:
+                    detector_str += "DETECTOR "
+                    for i, value in enumerate(row):
+                        if value == 1:
+                            detector_str += f"rec[{-((num_repeat-round_num+1)*stab_size*2)+i}] rec[{-((num_repeat-round_num)*stab_size*2)+i}] "
+                    detector_str += "\n"
+            else:
+                for row in aggregate_matrix:
+                    detector_str += "DETECTOR "
+                    for i, value in enumerate(row):
+                        if value == 1:
+                            detector_str += f"rec[{-((num_repeat-round_num+1)*stab_size*2)+i+stab_size}] rec[{-((num_repeat-round_num)*stab_size*2)+i+stab_size}] "
+                    detector_str += "\n"
+    detector_circuit = stim.Circuit(detector_str) 
+    circuit += detector_circuit
+
+
+    for i in range(QRM.N):
+        circuit.append("X_ERROR" if z_basis else "Z_ERROR", i, p)
+        circuit.append("M" if z_basis else "MX", i)
+    round_circuit.append("TICK")
+
+
+    detector_str = ""
+    if z_basis:
+        for i, row in enumerate(QRM.SZ):
+            detector_str += "DETECTOR "
+            for k, value in enumerate(aggregate_matrix[i]):
+                if value == 1:
+                    detector_str += f"rec[{-(QRM.N+stab_size*2)+k}] "
+            for j, value in enumerate(row):                                     
+                if value==1:                                                    
+                    detector_str +=f"rec[{j-QRM.N}] "                               
+            detector_str += "\n"
+    observable_str = ""
+    if z_basis:
+        for i, row in enumerate(QRM.LZ):
+            observable_str += f"OBSERVABLE_INCLUDE({i}) "
+            for j, value in enumerate(row):
+                if value == 1:
+                    observable_str += f"rec[{j-QRM.N}] "
+            observable_str += "\n"
+    detector_circuit = stim.Circuit(detector_str) 
+    circuit += detector_circuit
+        
+    observable_circuit = stim.Circuit(observable_str)
+    circuit+= observable_circuit
+
+    return circuit
+
+
+def dict_to_csc_matrix(elements_dict, shape):
+    # Constructs a `scipy.sparse.csc_matrix` check matrix from a dictionary `elements_dict` 
+    # giving the indices of nonzero rows in each column.
+    nnz = sum(len(v) for k, v in elements_dict.items())
+    data = np.ones(nnz, dtype=np.uint8)
+    row_ind = np.zeros(nnz, dtype=np.int64)
+    col_ind = np.zeros(nnz, dtype=np.int64)
+    i = 0
+    for col, v in elements_dict.items():
+        for row in v:
+            row_ind[i] = row
+            col_ind[i] = col
+            i += 1
+    return csc_matrix((data, (row_ind, col_ind)), shape=shape)
+
+def dem_to_check_matrices(dem: stim.DetectorErrorModel, return_col_dict=False):
+
+    DL_ids: Dict[str, int] = {} # detectors + logical operators
+    L_map: Dict[int, FrozenSet[int]] = {} # logical operators
+    priors_dict: Dict[int, float] = {} # for each fault
+
+    def handle_error(prob: float, detectors: List[int], observables: List[int]) -> None:
+        dets = frozenset(detectors)
+        obs = frozenset(observables)
+        key = " ".join([f"D{s}" for s in sorted(dets)] + [f"L{s}" for s in sorted(obs)])
+
+        if key not in DL_ids:
+            DL_ids[key] = len(DL_ids)
+            priors_dict[DL_ids[key]] = 0.0
+
+        hid = DL_ids[key]
+        L_map[hid] = obs
+#         priors_dict[hid] = priors_dict[hid] * (1 - prob) + prob * (1 - priors_dict[hid])
+        priors_dict[hid] += prob
+
+    for instruction in dem.flattened():
+        if instruction.type == "error":
+            dets: List[int] = []
+            frames: List[int] = []
+            t: stim.DemTarget
+            p = instruction.args_copy()[0]
+            for t in instruction.targets_copy():
+                if t.is_relative_detector_id():
+                    dets.append(t.val)
+                elif t.is_logical_observable_id():
+                    frames.append(t.val)
+            handle_error(p, dets, frames)
+        elif instruction.type == "detector":
+            pass
+        elif instruction.type == "logical_observable":
+            pass
+        else:
+            raise NotImplementedError()
+    check_matrix = dict_to_csc_matrix({v: [int(s[1:]) for s in k.split(" ") if s.startswith("D")] 
+                                       for k, v in DL_ids.items()},
+                                      shape=(dem.num_detectors, len(DL_ids)))
+    observables_matrix = dict_to_csc_matrix(L_map, shape=(dem.num_observables, len(DL_ids)))
+    priors = np.zeros(len(DL_ids))
+    for i, p in priors_dict.items():
+        priors[i] = p
+
+    if return_col_dict:
+        return check_matrix, observables_matrix, priors, DL_ids
+    return check_matrix, observables_matrix, priors
 
                     
